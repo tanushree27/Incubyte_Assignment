3ce78f4e81a585f2185fd664815ff7839fca2e12
Tanushree Tumane <tanushreetumane@gmail.com>
2020-12-22 13:58:01 +0530

    Handled Any number test
    Tests:
    1) Empty String - Pass
    2) Single Number String - Pass
    3) Two Number String - Pass
    4) Any Number String - Pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -5,9 +5,13 @@ public class StringCalculator {
             return 0;
 
         String[] numbers = str.split(",");
-        int num1 = Integer.parseInt(numbers[0]);
-        int num2 = numbers.length == 2 ? Integer.parseInt(numbers[1]): 0;
+        int sum = 0;
 
-        return num1 + num2;
+        for(String s : numbers)
+        {
+            sum += Integer.parseInt(s);
+        }
+
+        return sum;
     }
 }
