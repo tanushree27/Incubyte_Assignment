f1f599741c2576b01875f137dea35df45fc762af
Tanushree Tumane <tanushreetumane@gmail.com>
2020-12-22 16:18:25 +0530

    Handled support for different delimiters test
    Tests:
    1) Empty String - Pass
    2) Single Number String - Pass
    3) Two Number String - Pass
    4) Any Number String - Pass
    5) New lines between numbers (instead of commas) - Pass
    6) Support for different delimiters - Pass

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,11 +1,31 @@
+import java.util.regex.*;
 public class StringCalculator {
 
     public int add (String str) {
         if (str.length() == 0)
             return 0;
 
-        str = str.replaceAll("\n", ",");
-        String[] numbers = str.split(",");
+        String defaultDelimiter = ",";
+        String customDelimiter;
+        boolean isDelimiterPresent = false;
+        String[] numbers = new String[str.length()];
+
+        if(str.startsWith("//"))
+        {
+            isDelimiterPresent = true;
+            Matcher m = Pattern.compile("//(.)\n(.*)").matcher(str);
+            m.matches();
+            customDelimiter = m.group(1);
+            String numbersLeft = m.group(2);
+            numbers = numbersLeft.split(customDelimiter);
+        }
+
+        if(!isDelimiterPresent)
+        {
+            str = str.replaceAll("\n", defaultDelimiter);
+            numbers = str.split(defaultDelimiter);
+        }
+
         int sum = 0;
 
         for(String s : numbers)
